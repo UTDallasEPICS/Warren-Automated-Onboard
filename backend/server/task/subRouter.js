8aafe94a36e3d793fc2f203e71dbf996c0351772
ssamikshaa <samikshaa.saiprakash@gmail.com>
2024-01-31 15:21:12 -0600

    changes to task api

--- backend/server/task/subRouter.js
+++ backend/server/task/subRouter.js
@@ -1,8 +1,16 @@
-// const controller = require('./taskController');
-// const addRoutes = (router) => {
-//     router.post('/task', controller.addTask);
-//     router.get('/departments/tasks', controller.getAllDepartmentsTasks)
-//     router.get('/department/tasks', controller.getAllDepartmentTasks)
-//     router.get('/users/tasks', controller.getAllUsersTasks)
-//     router.get('/user/tasks', controller.getAllUserTasks)
-// }
+const controller = require('./taskController');
+const addRoutes = (router) => {
+    router.post('/task', controller.addTask);
+
+    router.get('/task/:id', controller.getTaskByID);
+    router.get('/tasks', controller.getAllTasks);
+    router.delete('/task/:id', controller.deleteTask);
+
+   // router.get('/departments/tasks', controller.getAllDepartmentsTasks)
+   // router.get('/department/tasks', controller.getAllDepartmentTasks)
+   // router.get('/users/tasks', controller.getAllUsersTasks)
+   // router.get('/user/tasks', controller.getAllUserTasks)
+}
+module.exports = {
+    addRoutes,
+  };
--- backend/server/task/taskController.js
+++ backend/server/task/taskController.js
@@ -1,7 +1,88 @@
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+// import { PrismaClient } from '@prisma/client'
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 module.exports = {
     // Create a new task
-}
+    addTask: async(req, res) => {
+        const { desc } = req.body;
+        try{
+            const task = await prisma.task.create({
+                data: {
+                    description: desc,
+                    archived: false,
+                },
+            });
+            res.status(201).json({ message: 'Task added successfully' });
+        }
+        catch(error){
+            console.log(error);
+            res.status(500).json({ message: 'Error adding task' });
+        }
+        
+    },
+
+    getTaskByID: async(req, res) => {
+        const {id} = req.params;
+        try {
+            const task = await prisma.task.findUnique({
+              where: {
+                archived: false,
+                id: parseInt(id),
+              },
+            });
+            
+            if (!task) {
+              return res.status(404).json({ message: 'Task not found or is archived' });
+            }
+            return res.status(200).json(task)
+        }
+        catch (error) {
+            console.log(error);
+            res.status(500).json({ message: 'Error retrieving task' });
+        }
+    },
+
+    getAllTasks: async(req, res) => {
+        try {
+            const tasks = await prisma.task.findMany({
+              where: {
+                archived: false,
+              },
+            });
+      
+            if (!tasks) {
+              return res
+                .status(404).json({ message: 'No tasks found' });
+            }
+            res.status(200).json(tasks);
+        }
+        catch (error) {
+            console.log(error);
+            res.status(500).json({ message: 'Error retrieving all tasks' });
+        }
+    },
+
+    deleteTask: async(req, res) => {
+        const { id } = req.params;
+        try {
+            const deletedTask = await prisma.task.update({
+                where: {
+                id: parseInt(id),
+              },
+            data: {
+                archived: true,
+              },
+            });
+            
+            if (!deletedTask) {
+              return res.status(404).json({ message: 'Task not found or is archived' });
+            }
+            res.status(200).json({ message: 'Task deleted successfully' });
+        }
+        catch (error) {
+            console.log(error);
+            res.status(500).json({ message: 'Error deleting task' });
+        }
+    }
+};
